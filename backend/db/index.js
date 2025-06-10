1d60a6bd2c5d4654a8ef42baa9df6847c26a1433
nilesh-developer <nileshchauhan5911@gmail.com>
2025-06-10 19:55:50 +0530

    changed db to eazzy

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const dbConnect = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/eazzy-site`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/eazzy`) // use eazzy-site when new payment gateway is used
         console.log("MONGODB Connected Successfully. DB HOST:", connectionInstance.connection.host)
     } catch (error) {
         console.log("MONGODB Connection Failed. Error: ", error)
